c920fc450f93e75abb59b41241f510a014559e7d
Carl Baker <carlbaker@gmail.com>
2024-01-03 13:40:48 -0700

    running through nextjs tutorial chapter 5.

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Roboto, Lusitana } from 'next/font/google';
+
+
+export const roboto = Roboto(
+    {weight: ["300", "500", "700"], subsets: ["latin"]}
+);
+export const lusitana = Lusitana(
+    {weight: ["400", "700"], subsets: ["latin"]}
+);
